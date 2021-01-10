af25f58a9f06c90f03a51d8d0d9aebd2006acb5e
yuzden <yuzden.4729@gmail.com>
2021-01-10 17:56:44 +0200

    Add information about files

--- SupportFunctions.cpp
+++ SupportFunctions.cpp
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 3
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Yuzden Karadalieva
+* @idnumber 62566
+* @compiler VC
+*
+* <file with helper functions>
+*
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
